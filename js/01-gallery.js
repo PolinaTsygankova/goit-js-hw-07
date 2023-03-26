7b751300f65e367bd857727678832b02c667dd23
PolinaTsugankova <pp6056512@gmail.com>
2023-03-26 17:08:25 +0300

    add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const photosMarkup = addImagesItems(galleryItems);
+gallery.insertAdjacentHTML("beforeend", photosMarkup);
+
+function addImagesItems(galleryItems) {
+   return galleryItems
+      .map(({ preview, original, description }) => {
+         return `
+      <li class="gallery__item">
+        <a class="gallery__link" target="_self" href="${original}">
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+        </a>
+      </li>`;
+      })
+      .join("");
+}
+
+// натискаємо тільки на фотографію (перевірка іф), і потім добавляємо та знімаємо на фото, бекграунд або ескейп
+
+gallery.addEventListener("click", onImgClick);
+
+function onImgClick(evt) {
+   evt.preventDefault();
+
+   if (evt.target.nodeName !== "IMG") {
+      return;
+   } else {
+      const photoLink = evt.target.dataset.source;
+
+      const instance = basicLightbox
+         .create(`<img width="1400" height="900" src=${photoLink}>`)
+         .show();
+   }
+}
+
+document.addEventListener("keydown", onImgClickClose);
+
+function onImgClickClose(evt) {
+   if (evt.code === "Escape") {
+      const photoLink = evt.target.dataset.source;
+
+      basicLightbox
+         .create(`<img width="1400" height="900" src=${photoLink}>`)
+         .close();
+   }
+}
